a4a0f68db7a7407ae879d564fd19894b18d6c8e7
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-07 20:33:23 +0500

    Delete day00a

--- day00a/main.js
+++
@@ -1,2 +0,0 @@
-var persons_name = "Jameel"; // Store the persons name.
-console.log("Hey ".concat(persons_name, ", Do you want to have nehari tommorow afternoon?")); // Print a message to said person.
--- day00a/main.ts
+++
@@ -1,3 +0,0 @@
-let persons_name = "Jameel"; // Store the persons name.
-
-console.log(`Hey ${persons_name}, Do you want to have nehari tommorow afternoon?`); // Print a message to said person.
