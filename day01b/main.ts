93fcbc36890adc66f39d42a9fc4fa487b81eb983
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-07 20:46:56 +0500

    Day 02 Q:2

---
+++ day01b/main.ts
@@ -0,0 +1,3 @@
+const famousPerson = "Walt Disney"; // Store the name of the famous person in a variable.
+
+console.log(`${famousPerson} once said "It's fun to do the impossible."`); // Concatinate the name of the famous person with their quote and print it.
