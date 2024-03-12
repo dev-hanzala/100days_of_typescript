e6d4b33142a8572949ce465d9862ae16572a7bab
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-12 11:01:28 +0500

    day 11 q1

---
+++ day11a/main.js
@@ -0,0 +1,10 @@
+var users = [];
+if (users.length != 0) {
+    users.forEach(function (users) {
+        console.log("Hello ".concat(users));
+    });
+}
+else {
+    console.log("Gotta get more users.");
+}
+;
