2b1a618c024c73065534ef65fd5d31cec5345e0e
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-17 11:17:02 +0500

    day 16 q2

---
+++ day16b/main.js
@@ -0,0 +1,10 @@
+var laptops = [
+    { maker: "HP", model: "Pavilion Aero 13", year: 2021 },
+    { maker: "Lenovo", model: "Thinkpad X1 Carbon Gen 3", year: 2013 },
+    { maker: "Razor", model: "Stealth 13", year: 2019 },
+    { maker: "Asus", model: "Zephyrus G14", year: 2022 },
+    { maker: "Framework", model: "laptop 13", year: 2021 }
+];
+var laptop1 = laptops[0], laptop2 = laptops[1];
+console.log(laptop1);
+console.log(laptop2);
---
+++ day16b/main.ts
@@ -0,0 +1,14 @@
+let laptops:object[] = [
+	{maker: "HP", model: "Pavilion Aero 13", year: 2021},
+	{maker: "Lenovo", model: "Thinkpad X1 Carbon Gen 3", year: 2013},
+	{maker: "Razor", model: "Stealth 13", year: 2019},
+	{maker: "Asus", model: "Zephyrus G14", year: 2022},
+	{maker: "Framework", model: "laptop 13", year: 2021}
+];
+
+let [laptop1, laptop2] = laptops;
+console.log(laptop1);
+console.log(laptop2);
+
+
+
