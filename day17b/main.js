ff51f74a2ebd04ba557c4d71969ec915eda9e8a1
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-18 10:55:08 +0500

    day 17 q2

---
+++ day17b/main.js
@@ -0,0 +1,2 @@
+var myIdealDay = "My Ideal day would be:\n1. Waking up early and excercising.\n2. Drinking 2 leters of water.\n3. Learning to code.";
+console.log(myIdealDay);
---
+++ day17b/main.ts
@@ -0,0 +1,6 @@
+let myIdealDay = `My Ideal day would be:
+1. Waking up early and excercising.
+2. Drinking 2 leters of water.
+3. Learning to code.`;
+console.log(myIdealDay);
+
