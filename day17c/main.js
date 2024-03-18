4a97fcf88d5ec7e1835eff67d150dffbec176e96
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-18 11:00:59 +0500

    day 17 q3

---
+++ day17c/main.js
@@ -0,0 +1,5 @@
+function areaCalculator(width, height) {
+    return width * height;
+}
+var areaCalculatorArrow = function (width, height) { return width * height; };
+console.log(areaCalculatorArrow(7, 9));
---
+++ day17c/main.ts
@@ -0,0 +1,8 @@
+function areaCalculator(width:number, height:number) {
+	return width * height;
+}
+
+let areaCalculatorArrow = (width:number, height:number) => width*height;
+
+console.log(areaCalculatorArrow(7,9));
+
