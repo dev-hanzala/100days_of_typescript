cd5d562bf1a2817abe14e00eaaab2ef0f5135877
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-19 12:46:20 +0500

    day 18 q1

---
+++ day18a/main.js
@@ -0,0 +1,11 @@
+var smartphone = {
+    manifacturor: "Asus",
+    name: "Zenfone 10",
+    year: 2023,
+    specifications: {
+        ram: "16GB",
+        storage: "512GB",
+        battery: "4500mah"
+    }
+};
+console.log(smartphone);
