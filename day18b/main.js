df165f01777e8e99ff1c35076d53af2b9a0c2642
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-19 13:04:03 +0500

    day 18 q2

---
+++ day18b/main.js
@@ -0,0 +1,7 @@
+var developerSkills = {
+    languages: ["Typescript", "Javascript", "Python"],
+    frameworks: ["React", "Vue", "NextJS"],
+    tools: ["Git", "Docker", "Nodejs"]
+};
+var languages = developerSkills.languages, frameworks = developerSkills.frameworks, tools = developerSkills.tools;
+console.log("Language: ".concat(languages[1], ", Framework: ").concat(frameworks[2], ", Tool: ").concat(tools[2], "."));
---
+++ day18b/main.ts
@@ -0,0 +1,9 @@
+let developerSkills = {
+	languages: ["Typescript", "Javascript", "Python"],
+	frameworks: ["React", "Vue", "NextJS"],
+	tools: ["Git", "Docker", "Nodejs"]
+};
+
+let { languages, frameworks, tools } = developerSkills;
+
+console.log (`Language: ${languages[1]}, Framework: ${frameworks[2]}, Tool: ${tools[2]}.`);
