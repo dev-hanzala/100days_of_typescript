ab4aa66631289266e73730ec0e9dba4a962580e9
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-20 19:17:21 +0500

    day 19 q1

---
+++ day19a/main.js
@@ -0,0 +1,3 @@
+var numberList = [1, 3, 5, 7, 9];
+var doubledNumberList = numberList.map(function (number) { return number * 2; });
+console.log(doubledNumberList);
---
+++ day19a/main.ts
@@ -0,0 +1,5 @@
+let numberList = [1,3,5,7,9];
+
+let doubledNumberList = numberList.map(number => number*2);
+
+console.log(doubledNumberList);
