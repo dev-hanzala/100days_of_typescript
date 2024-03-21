ca2d136ac74603649e5dcc8b21861b3d5f0ca9d8
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-21 18:04:34 +0500

    day 04 q1

---
+++ day04a/first.js
@@ -0,0 +1,4 @@
+// Hanzala Waseem 7th of March 2024
+// This code prints a message to my friend.
+var friendsName = "Jameel"; // Store the persons name.
+console.log("Hey ".concat(friendsName, ", Do you want to have nehari tommorow afternoon?")); // Print a message to said person.
