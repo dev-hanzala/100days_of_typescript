f61a8ead9a20898d9dc2ca4fd0153b0603fa8db8
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-23 17:42:30 +0500

    day 22 q3

---
+++ day22c/main.js
@@ -0,0 +1,6 @@
+function checkBothTrue(conditionA, conditionB) {
+    var result = conditionA && conditionB;
+    return result;
+}
+;
+console.log(checkBothTrue(true, false));
---
+++ day22c/main.ts
@@ -0,0 +1,6 @@
+function checkBothTrue(conditionA: boolean, conditionB: boolean): boolean{
+	let result = conditionA && conditionB;
+	return result;
+};
+
+console.log(checkBothTrue(true,false));
