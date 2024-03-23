012f388d0d68dd0d6ca3eaf64ffdf68fc67d03bc
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-23 17:38:48 +0500

    day 22 q2

---
+++ day22b/main.js
@@ -0,0 +1,6 @@
+function remainder(num1, num2) {
+    var answer = num1 % num2;
+    return answer;
+}
+;
+console.log(remainder(5, 2));
---
+++ day22b/main.ts
@@ -0,0 +1,6 @@
+function remainder(num1: number, num2: number): number{
+	let answer = num1 % num2;
+	return answer;
+};
+
+console.log(remainder(5,2));
