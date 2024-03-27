f8b8add77ae8bf95fcf9ed68593988ece9fdeaed
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-27 20:51:43 +0500

    day 26 q1

---
+++ day26a/main.js
@@ -0,0 +1,5 @@
+// function addTwoNumbers(num1:number, num2:number){
+// 	return num1 + num2;
+// }
+var addTwoNumbers = function (num1, num2) { return num1 + num2; };
+console.log(addTwoNumbers(1, 6));
---
+++ day26a/main.ts
@@ -0,0 +1,7 @@
+// function addTwoNumbers(num1:number, num2:number){
+// 	return num1 + num2;
+// }
+
+let addTwoNumbers = (num1,num2) => num1 + num2;
+
+console.log(addTwoNumbers(1,6));
