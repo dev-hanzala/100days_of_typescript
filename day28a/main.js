25981be849c3b8cdabe34636472239fedf186c7a
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-29 19:06:30 +0500

    day 28 q1

---
+++ day28a/main.js
@@ -0,0 +1,5 @@
+function getLength(string) {
+    return string.length;
+}
+;
+console.log(getLength("Hello world"));
---
+++ day28a/main.ts
@@ -0,0 +1,5 @@
+function getLength(string:string){
+	return string.length;
+};
+
+console.log(getLength("Hello world"));
