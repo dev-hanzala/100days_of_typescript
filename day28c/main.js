c21f2aca204664909aa18f12c41223764dbab43b
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-29 19:21:26 +0500

    day 28 q3

---
+++ day28c/main.js
@@ -0,0 +1,4 @@
+function replaceJavascriptWithTypescript(string) {
+    console.log(string.replace(/Javascript/g, "Typescript"));
+}
+replaceJavascriptWithTypescript("I like Javascript.");
---
+++ day28c/main.ts
@@ -0,0 +1,5 @@
+function replaceJavascriptWithTypescript(string:string){
+	console.log(string.replace(/Javascript/g, "Typescript"));	
+}
+
+replaceJavascriptWithTypescript("I like Javascript.");
