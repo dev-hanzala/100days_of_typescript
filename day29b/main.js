1bbc4765aab21645af749e035d0c31895ec68624
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-03-30 21:53:48 +0500

    day 29 q2

---
+++ day29b/main.js
@@ -0,0 +1,6 @@
+"use strict";
+function checkPresence(string) {
+    return string.includes("JavaScript");
+}
+;
+console.log(checkPresence("I love JavaScript."));
---
+++ day29b/main.ts
@@ -0,0 +1,5 @@
+function checkPresence(string: string){
+	return string.includes("JavaScript");
+};
+
+console.log(checkPresence("I love JavaScript."));
