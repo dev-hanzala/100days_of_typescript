c41da59a31448c7ad63ad9ba9527b2e206f0d1d7
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-04-02 13:20:31 +0500

    day 20 q1

---
+++ day30a/main.js
@@ -0,0 +1,5 @@
+function roundInt(value) {
+    return Math.round(value);
+}
+;
+console.log(roundInt(1.4));
---
+++ day30a/main.ts
@@ -0,0 +1,5 @@
+function roundInt(value: number): number {
+	return Math.round(value);
+};
+
+console.log(roundInt(1.4));
