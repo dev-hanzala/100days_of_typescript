af921411044947f8fc172dda21f952ccb8c3d933
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-04-02 22:27:44 +0500

    day 32 q2

---
+++ day32b/main.ts
@@ -0,0 +1,5 @@
+function numbersFilteredForGreaterThanTen(numbers: number[]){
+	return numbers.filter(numbers => numbers > 10);
+};
+
+console.log(numbersFilteredForGreaterThanTen([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]));
