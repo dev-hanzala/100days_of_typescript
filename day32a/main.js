1585e76ca80544dcc01e02684b006f5307bf4504
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-04-02 22:23:32 +0500

    day 32 q1

---
+++ day32a/main.js
@@ -0,0 +1,3 @@
+var words = ["time", "space", "light", "matter"];
+var lengths = words.map(function (words) { return words.length; });
+console.log(lengths);
---
+++ day32a/main.ts
@@ -0,0 +1,5 @@
+let words = ["time", "space", "light", "matter"];
+
+let lengths = words.map(words => words.length);
+
+console.log(lengths);
