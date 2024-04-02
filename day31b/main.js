6be1820ece716c22d92caab517bd247c8205b03b
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-04-02 13:49:40 +0500

    day 31 q2

---
+++ day31b/main.js
@@ -0,0 +1,5 @@
+function removeLastElement(array) {
+    return array.pop();
+}
+;
+console.log(removeLastElement([1, 2, 3, 4, 5]));
---
+++ day31b/main.ts
@@ -0,0 +1,5 @@
+function removeLastElement(array: any[]): any{
+	return array.pop();
+};
+
+console.log(removeLastElement([1,2,3,4,5]));
