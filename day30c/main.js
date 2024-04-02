3ed43438782ca7e3cfd8fd907477ee58d6b3033f
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-04-02 13:37:58 +0500

    day 30 q3

---
+++ day30c/main.js
@@ -0,0 +1,6 @@
+function isValueNaN(value) {
+    return isNaN(value);
+}
+;
+console.log(isValueNaN("Hello world!"));
+console.log(isValueNaN(50));
---
+++ day30c/main.ts
@@ -0,0 +1,7 @@
+function isValueNaN(value: any): boolean {
+	return isNaN(value);
+};
+
+console.log(isValueNaN("Hello world!"));
+
+console.log(isValueNaN(50));
