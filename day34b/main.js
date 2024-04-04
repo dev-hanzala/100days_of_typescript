f1d1731f1b8115e4fa6b0fbe0fc966b6f6af5aea
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-04-04 22:02:28 +0500

    day 34 q2

---
+++ day34b/main.js
@@ -0,0 +1,2 @@
+var randomNuberBW1and10 = Math.floor(Math.random() * 10) + 1;
+console.log(randomNuberBW1and10);
---
+++ day34b/main.ts
@@ -0,0 +1,3 @@
+let randomNuberBW1and10 = Math.floor(Math.random() * 10) + 1;
+
+console.log(randomNuberBW1and10);
