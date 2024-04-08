29cc52b83ad1508e1188a337473d251541e4e363
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-04-09 00:08:48 +0500

    day 38 q1

---
+++ day38a/main.js
@@ -0,0 +1,6 @@
+"use strict";
+const map = new Map();
+map.set("Japan:", "Tokyo");
+map.set("China", "Beijing");
+map.set("USA", "Washington DC");
+console.log(map);
---
+++ day38a/main.ts
@@ -0,0 +1,9 @@
+const map = new Map<string, string>();
+
+map.set("Japan:", "Tokyo");
+
+map.set("China", "Beijing");
+
+map.set("USA", "Washington DC");
+
+console.log(map);
