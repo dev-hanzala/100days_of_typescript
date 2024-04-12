af536a49cb1d8a426e31a501523a0caaa334736c
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-04-12 22:10:23 +0500

    day 40 q3

---
+++ day40c/main.js
@@ -0,0 +1,6 @@
+var colors = ["red", "green", "yellow"];
+for (var _i = 0, colors_1 = colors; _i < colors_1.length; _i++) {
+    var color = colors_1[_i];
+    console.log(color);
+}
+;
---
+++ day40c/main.ts
@@ -0,0 +1,5 @@
+const colors = ["red", "green", "yellow"];
+
+for(const color of colors) {
+	console.log(color);
+};
