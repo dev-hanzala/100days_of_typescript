1ccb0f3b8ad3cb17504b7f3fc8690fb0c3a1ba87
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-04-14 19:53:55 +0500

    day 43 q1

---
+++ day43a/main.js
@@ -0,0 +1,6 @@
+var normalFunction = function (a, b) {
+    return a + b;
+};
+var arrowFunction = function (a, b) { return a + b; };
+console.log(normalFunction(1, 2));
+console.log(arrowFunction(1, 2));
---
+++ day43a/main.ts
@@ -0,0 +1,9 @@
+const normalFunction = function(a,b){
+	return a+b;
+}
+
+const arrowFunction = (a,b) => a+b;
+
+console.log(normalFunction(1,2));
+
+console.log(arrowFunction(1,2));
