6daf43443482954e5f039bf27671c9da6de85577
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-04-14 19:46:04 +0500

    day 42 q2

---
+++ day42b/main.js
@@ -0,0 +1,8 @@
+var rectangle = {
+    length: 4,
+    width: 5,
+    calculateArea: function () {
+        return this.length * this.width;
+    },
+};
+console.log(rectangle.calculateArea());
---
+++ day42b/main.ts
@@ -0,0 +1,9 @@
+const rectangle = {
+	length: 4,
+	width: 5,
+	calculateArea: function () {
+	  return this.length * this.width;
+	},
+};
+
+console.log(rectangle.calculateArea());
