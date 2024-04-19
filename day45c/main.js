49e529b2d3e74a56c3f5f745b0991fa8e05eaa0e
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-04-19 21:00:05 +0500

    day 45 q3

---
+++ day45c/main.js
@@ -0,0 +1,7 @@
+var person = {
+    name: "John",
+    age: 17,
+    favourateColor: "Black",
+};
+var json = JSON.stringify(person, null, 3);
+console.log(json);
---
+++ day45c/main.ts
@@ -0,0 +1,8 @@
+const person = {
+	name: "John",
+	age: 17,
+	favourateColor: "Black",
+ };
+ const json = JSON.stringify(person, null, 3);
+ console.log(json);
+ 
